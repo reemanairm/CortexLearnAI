157e2f86a963bf6300a1fc9cd9212d6dd2c54fc8
reemanairm <reemanairm@gmail.com>
2026-03-28 20:01:26 +0530

    fix4

--- Front-End/AI-Study-Partner/src/utils/apiPaths.js
+++ Front-End/AI-Study-Partner/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || (import.meta.env.MODE === 'production' ? "" : "http://localhost:8000");
+export const BASE_URL = import.meta.env.VITE_BASE_URL || (import.meta.env.MODE === 'production' ? "" : "https://cortexlearnai.onrender.com");
 
 export const API_PATHS = {
   AUTH: {
