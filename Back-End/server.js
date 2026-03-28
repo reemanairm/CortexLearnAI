9a6df7d2a317c50377ca87aad89df6cd3aadf6b7
reemanairm <reemanairm@gmail.com>
2026-03-28 14:26:30 +0530

    fix express catch all route for render

--- Back-End/server.js
+++ Back-End/server.js
@@ -35,7 +35,7 @@ const startServer = async () => {
 
     app.use(express.static(path.join(__dirname,"../Front-End/dist")));
 
-    app.get("*",(req, res) => {
+    app.get(/.*/,(req, res) => {
       res.sendFile(path.join(__dirname,"../Front-End/dist/index.html"));
     });
 
