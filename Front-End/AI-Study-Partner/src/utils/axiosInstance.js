825ed05d2035e4d4d60f509b8d8d1113b57e1bcf
reemanairm <reemanairm@gmail.com>
2026-03-28 20:35:53 +0530

    fix8

--- Front-End/AI-Study-Partner/src/utils/axiosInstance.js
+++ Front-End/AI-Study-Partner/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_URL } from "./apiPaths";
+
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
   timeout: 80000,
   headers: {
     "Content-Type": "application/json",
